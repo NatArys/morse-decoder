9902851c6322198ef6053d0023ca5381f0cb9a81
NatArys <91931609+NatArys@users.noreply.github.com>
2023-01-27 15:29:05 +0700

    feat: add decoding

--- src/index.js
+++ src/index.js
@@ -38,7 +38,12 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = '';
+    for (let i = 0; i < expr.length; i += 10) {
+        let symbol = expr.substring(i, i + 10);
+        result += (symbol === '**********') ? ' ' : MORSE_TABLE[symbol.replace(/^0+/, '').replace(/(10)/g, '.').replace(/(11)/g, '-')];
+    }
+    return result;
 }
 
 module.exports = {
